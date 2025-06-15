d2df039e948012ce999ec2755f9104ddcae983c0
rahmanov123 <rahmanovfarid5@gmail.com>
2025-06-15 16:15:24 +0300

    Update brain-prime.js

--- code/src/games/brain-prime.js
+++ code/src/games/brain-prime.js
@@ -1,5 +1,31 @@
-#!/usr/bin/env node
+import { createRound, createGame } from '../index.js';
+import { getRandom } from '../helpers.js';
 
-import runGame from '../src/games/brain-prime.js';
+function isPrime(number) {
+  if (number < 2) {
+    return false;
+  }
 
-runGame();
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function initRound() {
+  const number = getRandom(1, 50);
+
+  const question = String(number);
+  const answer = isPrime(number) ? 'yes' : 'no';
+
+  return createRound(question, answer);
+}
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+export default function runGame() {
+  createGame(description, initRound);
+}
