57b15ad3a24d84d6b62a7951d3f33f881258d5bb
rahmanov123 <rahmanovfarid5@gmail.com>
2025-06-15 16:15:02 +0300

    Update brain-gcd.js

--- code/src/games/brain-gcd.js
+++ code/src/games/brain-gcd.js
@@ -1,5 +1,23 @@
-#!/usr/bin/env node
+import { createRound, createGame } from '../index.js';
+import { getRandom } from '../helpers.js';
 
-import runGame from '../src/games/brain-gcd.js';
+function findGcd(num1, num2) {
+  return num2 === 0 ? num1 : findGcd(num2, num1 % num2);
+}
 
-runGame();
+function initRound() {
+  const n1 = getRandom(1, 50);
+  const n2 = getRandom(1, 50);
+  const gcd = findGcd(n1, n2);
+
+  const question = `${n1} ${n2}`;
+  const answer = String(gcd);
+
+  return createRound(question, answer);
+}
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+export default function runGame() {
+  createGame(description, initRound);
+}
