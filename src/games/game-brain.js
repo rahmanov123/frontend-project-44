235f18f3f6a4e65d98ba7e452e022ac133dd7e48
rahmanov123 <rahmanovfarid5@gmail.com>
2025-06-15 16:25:50 +0300

    Create game-brain.js

---
+++ src/games/game-brain.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync'
+
+export default () => {
+  console.log('Welcome to the Brain Games!')
+  const userName = readlineSync.question('May I have your name? ', {
+    defaultInput: 'User',
+  })
+  console.log(`Hello, ${userName}!`)
+}
